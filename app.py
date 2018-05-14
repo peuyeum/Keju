228a9d7d49a64dce566ea6db8147b22d0c5fca89
Rolly Maulana Awangga <rolly@awang.ga>
2018-05-14 08:39:51 +0700

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.route('/<gurih>')
+def hello_world(gurih):
+    return gurih
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/crot', methods=['POST'])
+def login():
+	return request.form['anu']
