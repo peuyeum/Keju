2c9b81e5a0121ff19c233195fe00e90eb71deaf2
YogiAdityaS <adityayogi711@gmail.com>
2018-05-16 21:11:14 +0700

    hari pertama yogi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request
+from scopus import Firstauth as FA
 app = Flask(__name__)
 
 @app.route('/<gurih>')
@@ -13,3 +14,7 @@ def show_post(post_id):
 @app.route('/crot', methods=['POST'])
 def login():
 	return request.form['anu']
+
+@app.route('/kelompok1/<a>')
+def house(a):
+    return FA.home(a)
