703f26edd901c7c82cf5785b35100814e02ae67b
miftahulhasanah <33749381+miftahulhasanah@users.noreply.github.com>
2018-05-17 23:17:41 +0700

    Update authlastname.py

--- scopus/authlastname.py
+++ scopus/authlastname.py
@@ -1,7 +1,9 @@
 from pyscopus import Scopus
 key = '31b7abf24cbe92b89fada3c60a6e6708'
 scopus = Scopus(key)
-def hell(a):
+def halo(a):
     return a+ "halo"
 def mendapatkan(b):
-    return b+"mendapatkan
+    return b+"mendapatkan"
+def Snapshot(c):
+    return c+"Snapshot"
