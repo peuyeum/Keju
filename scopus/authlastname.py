de250a78faa3ab8822dc844843e365a52b6619c1
AnggaDwitya17 <anggadwitya17@gmail.com>
2018-05-17 20:50:24 +0700

    Angga kedua

--- scopus/authlastname.py
+++ scopus/authlastname.py
@@ -1,5 +1,5 @@
 from pyscopus import Scopus
-key = '	31b7abf24cbe92b89fada3c60a6e6708'
+key = '31b7abf24cbe92b89fada3c60a6e6708'
 scopus = Scopus(key)
 def hell(a):
-    return a+ "haii"
+    return a+ "halo"
