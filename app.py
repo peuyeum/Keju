457eb8fe48c5e910745a9513ec9dc091aad65dab
Muhammad Hanafi <mhanafi09051998@gmail.com>
2018-05-18 17:20:52 +0700

    Menambahkan komentar pengembalian nilai

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/<gurih>')
 def hello_world(gurih):
-    return gurih
+    return gurih #mengembalikan nilai dari variale gurih
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
