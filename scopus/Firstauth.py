d7b6349ebeb74f5fabd7c4c49885b4cb3e5abcd2
faridariyantosaputra <frdariyanto@gmail.com>
2018-05-18 19:36:38 +0700

    oke

--- scopus/Firstauth.py
+++ scopus/Firstauth.py
@@ -3,4 +3,4 @@ key = '50312b4710a276ec9670642f0f455259'
 scopus = Scopus(key)
 
 def home(a):
-    return a+"Nurgivani, Velariza, Farid, Yogi Kelompok 1 web service dikelas 2B"
+    return a+"HARI KE-2 PUASA"
