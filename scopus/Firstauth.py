c7d1bfb827e76f55e1c34fd49d6d57ac5823ceec
YogiAdityaS <adityayogi711@gmail.com>
2018-05-18 21:08:43 +0700

    hari ketiga

--- scopus/Firstauth.py
+++ scopus/Firstauth.py
@@ -3,4 +3,4 @@ key = '50312b4710a276ec9670642f0f455259'
 scopus = Scopus(key)
 
 def home(a):
-    return a+"Kalian tahu tidak rasanya sedih? Kalau tidak, sini merantau.."
+    return a+"Kalian tahu tidak rasanya sedih? Kalau tidak, sini merantau..."
