e18a12b25abae88a708f2dd2e2d2462c5c47dc86
Velariza <velarizaa@gmail.com>
2018-05-19 22:43:19 +0700

    quotes of the day

--- scopus/Firstauth.py
+++ scopus/Firstauth.py
@@ -3,4 +3,4 @@ key = '50312b4710a276ec9670642f0f455259'
 scopus = Scopus(key)
 
 def home(a):
-    return a+"annyeong haseyo"
+    return a+"Kamu tau kenapa aku tetap setia walaupun berat? Karena aku pernah merasakan sakitnya diduakan"
