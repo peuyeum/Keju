f9587f7f50adaf85a1311ed68de21c82c3295d69
faridariyantosaputra <frdariyanto@gmail.com>
2018-05-19 22:15:13 +0700

    ok

--- scopus/Firstauth.py
+++ scopus/Firstauth.py
@@ -3,4 +3,4 @@ key = '50312b4710a276ec9670642f0f455259'
 scopus = Scopus(key)
 
 def home(a):
-    return a+"My Hobby & Moodbooster : Writing a novel and shortstory, Sleep, Eat, and Bangtan :) "
+    return a+"annyeong haseyo"
