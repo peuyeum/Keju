fb54494e19646533b04fbc279d8ca080a48e694c
Kurnia Sandi <kurniya097@gmail.com>
2018-05-19 22:06:01 +0700

    memperbaiki file di folder scopus

--- scopus/afiliasi.py
+++ scopus/afiliasi.py
@@ -1,7 +1,16 @@
+#proses import library pyscopus dari app
 from pyscopus import Scopus
 
-app = Scopus (__nama__)
-
+#Test Pencarian
+search_df = 
 def hom(a):
-	return a+" comment "
+    return a+" is Found"  #memanggil variable a dengan menambah nilai/value is Found"
 	
+	
+	
+
+
+	
+	
+	
+#Lanjutkan di bawah sini guys, jangan di atas
