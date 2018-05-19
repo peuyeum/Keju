fc8f045992d1062523a65a1d634c06e0f9a0036b
AnggaDwitya17 <anggadwitya17@gmail.com>
2018-05-19 21:43:02 +0700

    Hari Keempat

--- scopus/authlastname.py
+++ scopus/authlastname.py
@@ -1,8 +1,8 @@
 from pyscopus import Scopus
 key = '31b7abf24cbe92b89fada3c60a6e6708'
 scopus = Scopus(key)
-def halo(a):
-    return a+ "Hai kelompok 3 disini"
+def halau(a):
+    return a+ "halau"
 def mendapatkan(b):
     return b+"mendapatkan"
 def Snapshot(c):
