92a240a52effc56bf5ec2629005afb52573cb85d
AlwanSuryansah <alwansuryansah1@gmail.com>
2018-05-19 09:51:23 +0700

    menambahkan kata untuk penyakinkan

--- scopus/afiliasi.py
+++ scopus/afiliasi.py
@@ -3,5 +3,5 @@ from pyscopus import Scopus
 app = Scopus (__nama__)
 
 def hom(a):
-	return a+" Ganteng aku tuh gaes . Percaya tidak percaya ini nyata gaes"
+	return a+" Ganteng aku tuh gaes . Percaya tidak percaya ini nyata gaessss"
 	
