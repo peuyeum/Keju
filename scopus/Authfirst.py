ce307cff12af4fc849353e19d77146a6cd4be266
karnoviriki <karnoviriki@gmail.com>
2018-05-19 13:50:37 +0700

    menambahkan search_df =

--- scopus/Authfirst.py
+++ scopus/Authfirst.py
@@ -4,9 +4,11 @@ key = '1e499709c626679a80b27fc8a207ceb3'
 
 
 #Test Search
+search_df = 
 def cari(nama):
     return nama+" Bisa"  #memanggil variable nama dengan menambah value bisa
 
 #ini komentar
 #jangan menyerah pasti bisa
 #jangan kebanyakan komentar
+
