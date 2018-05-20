dabfaba795706914827745953d28494e38e6e901
Muhammad Hanafi <mhanafi09051998@gmail.com>
2018-05-20 13:29:03 +0700

    "Menambahkan Search_df"

--- scopus/Authfirst.py
+++ scopus/Authfirst.py
@@ -4,6 +4,8 @@ key = '1e499709c626679a80b27fc8a207ceb3'
 scopus = Scopus(key)
 
 #Test Search
+search_df = scopus.search("KEY(interdisciplinary collaboration)", count=30)
+
 search_df = 
 def cari(nama):
     return nama+" Bisa"  #memanggil variable nama dengan menambah value bisa
