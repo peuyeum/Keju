9ae24c49b3bf5053371cde45d4acd1231de7f7af
Nurgivani S. H <33891794+shnurgivani@users.noreply.github.com>
2018-05-20 16:12:08 +0700

    alasan kenapa aku kuliah

--- scopus/Firstauth.py
+++ scopus/Firstauth.py
@@ -3,4 +3,4 @@ key = '50312b4710a276ec9670642f0f455259'
 scopus = Scopus(key)
 
 def home(a):
-    return a+"Kamu tau kenapa aku tetap setia walaupun berat? Karena aku pernah merasakan sakitnya diduakan"
+    return a+"Kamu tau kenapa aku kuliah? karena jaman sekarang orang bergelar lebih dihormati dan dihargai dibanding dengan yang tidak. Hey! bukan berarti aku gila hormat! aku hanya tidak ingin dianggap sampah"
