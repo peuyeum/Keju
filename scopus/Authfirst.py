0882f341a5fbd88df96a0544128f67dbd1ce3e3f
Ajis Trigunawan <ajis.trigunawan$%@gmail.com>
2018-05-21 18:03:48 +0700

    update pake yang hanafi

--- scopus/Authfirst.py
+++ scopus/Authfirst.py
@@ -7,7 +7,7 @@ scopus = Scopus(key)
 search_df = scopus.search("KEY(interdisciplinary collaboration)", count=30)
 #print (seaarch_df.head(10))
 
-search_df = 
+search_df = scopus.author("AUTHLASTNAME(scott)", count=2)
 def cari(nama):
     return nama+"Pasti Bisa"  # untuk memanggil variable nama dengan menambah value pasti bisa
 
