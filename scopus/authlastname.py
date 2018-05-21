4864627a32ae4e8bb8799c9723554cd5f2e2e3bb
Fikri-Aldi-Nugraha <fikrialdinugraha@gmail.com>
2018-05-21 22:35:52 +0700

    Hari Keenam

--- scopus/authlastname.py
+++ scopus/authlastname.py
@@ -1,6 +1,11 @@
 from pyscopus import Scopus
 key = '31b7abf24cbe92b89fada3c60a6e6708'
 scopus = Scopus(key)
+
+#search_df = scopus.search_author("AUTHLASTNAME(Barney)", count=30)
+#print(search_df.head(10))
+def pencarian(namabelakang):
+    return namabelakang+ "Pencarian berdasarkan nama belakang"
 def guk(a):
     return a+ "guk"
 def mendapatkan(b):
