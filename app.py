867a176605b7873b9de6d587bbbf8855835a562e
Fransiscussinaga <fransiscussinaga0608@gmail.com>
2018-05-21 18:20:48 +0700

    kodingan terakhir frans menambahkan huruf a

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ def show_post(post_id):
 def login():
 	return request.form['anu']
 
-@app.route('/frans/<a>')
-def house(a):
-	return SA.home(a)
-
-@app.route('/Subjarea/<b>')
-def serah(b):
-	return SA.mendapatkan(b)
+@app.route('/scopus/ASubjarea<a>', methods=['GET'])
+def retrn(a):
+    return SA.mendapatkan(a)
