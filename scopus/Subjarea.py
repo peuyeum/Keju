49c576ca609b793e531e204b0c178800ed84a2ce
Fransiscussinaga <37574780+Fransiscussinaga@users.noreply.github.com>
2018-05-22 16:49:07 +0700

    Update Subjarea.py

--- scopus/Subjarea.py
+++ scopus/Subjarea.py
@@ -4,8 +4,5 @@ key = '28afb89a9b06c53a126edd118d079c63'
 scopus = Scopus(key)
 
 def cari(area): 
-    #search = scopus.search("KEY(interdisciplinary collaboration)", count=30) 
-    search_au = scopus.search_author("Subjarea(city)") 
-    #return print(search_df.head(10)) 
-    return print(search_au)
-  
+	search_df = scopus.search("SUBJAREA("+ area + ")", count=30)
+	return search_df.head(10)
