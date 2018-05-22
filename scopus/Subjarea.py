ab38cf8a7ee88a5076bbd3483ee61a36ee6465ae
Fransiscussinaga <37574780+Fransiscussinaga@users.noreply.github.com>
2018-05-22 14:07:37 +0700

    Update Subjarea.py

--- scopus/Subjarea.py
+++ scopus/Subjarea.py
@@ -1,8 +1,11 @@
+#import libary
 from pyscopus import Scopus
 key = '28afb89a9b06c53a126edd118d079c63'
 scopus = Scopus(key)
 
-def mendapatkan(a):
-   return a+"mendapatkan"
-  
+def cari(nama): 
+    #search = scopus.search("KEY(interdisciplinary collaboration)", count=30) 
+    search_au = scopus.search_author("AUTHLASTNAME(scott)") 
+    #return print(search_df.head(10)) 
+    return print(search_au)
   
