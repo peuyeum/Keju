3229304db82f031a495cd01595abca050956c5ea
Fransiscussinaga <37574780+Fransiscussinaga@users.noreply.github.com>
2018-05-22 13:05:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def show_post(post_id):
 def login():
 	return request.form['anu']
 
-@app.route('/scopus/ASubjarea<a>', methods=['GET'])
+@app.route('/scopus/Subjarea<a>', methods=['GET'])
 def return(a):
     return SA.mendapatkan(a)
